bb7e3956279e8f319f1193f57a8b870d0db2f22e
lschueler01 <70756689+lschueler01@users.noreply.github.com>
2024-12-13 16:58:04 +0100

    Update database.sql

--- database.sql
+++ database.sql
@@ -4,7 +4,8 @@ create table weatherdata(
     longitude --decimal -> DEC(size, d)
     timezone --> string
     timezone_offset --> shift in seconds from UTC -> INT
-    current_datetime --> UNIX datetime --> Convert to datetime!!!
+    current_datetime --> UNIX datetime --> Convert to datetime!!! (Select dateadd(S, current_datetime], '1970-01-01') FROM weatherdata?)
+    local_time -- UNIX time in local time zone
     current_temp -- Current temp in Kelvin -> configure API to have it in Celsius
     current_pressure -- hPa
     current_humidity -- %
@@ -12,4 +13,18 @@ create table weatherdata(
     weather_id -- conversion table from API
     weather_main -- string
     weather_description  -- long weather description -> String -> get in DE?
+)
+
+-- optional depending on api response
+create table weathercodes(
+    weather_id
+    weather_main
+    weather_description
+)
+
+-- optional depending on api response
+create table city_coordinates(
+    longitude
+    latitude
+    city_name
 )
